1676c6ffe73955a2571e9180d0377e88d473bbd7
Entongsu <ensu@ucsd.edu>
2026-02-06 14:15:23 -0800

    update rfs

---
+++ rfs/vite.config.js
@@ -0,0 +1,12 @@
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//   base: '/three-gltf-viewer/',
+// });
+
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/rfs/',   // 👈 MUST match repo name
+});
